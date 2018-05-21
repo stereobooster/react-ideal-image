5168c4852fa282bb8d43f85747a14f9808705123
stereobooster <stereobooster@gmail.com>
2018-05-21 12:15:25 +0200

    Add styleguidist

---
+++ styleguide.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  title: 'react-precious-image',
+  components: 'src/components/**/index.js',
+  skipComponentsWithoutExample: true,
+  assetsDir: 'other/images',
+  webpackConfig: {
+    module: {
+      rules: [
+        {
+          test: /\.jsx?$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+      ],
+    },
+    externals: {
+      react: 'react',
+      'react-dom': 'react-dom',
+      'prop-types': 'prop-types',
+      'react-waypoint': 'react-waypoint',
+    },
+  },
+}
